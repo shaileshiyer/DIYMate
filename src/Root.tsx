234ae1c7460fffb895984b89d5ba1aa42ac39ff9
Shailesh Vaidheeswaran Iyer <shailesh.iyer@rwth-aachen.de>
2024-02-03 20:30:59 +0100

    Adds global libraries and first page.
    Adds global state management libraries like zustand and react router.
    Also, created a basic color palette for the app. Created a basic root
    page with no functionality just the title and subttitle.

---
+++ src/Root.tsx
@@ -0,0 +1,9 @@
+
+export default function Root() {
+    return (
+        <div id="home">
+            <h1>DIY-Tutorial-Mate</h1>
+            <p>An LLM-powered Text editor to help you write a DIY tutorial</p>
+        </div>
+    );
+}
